243576d66a2e6ce0b761d505c6f9b712e2665fa3
monicaGY <monicagarciayadaicela@gmail.com>
2023-06-29 19:40:40 +0200

    Crear base de datos

---
+++ src/Entity/Interaction.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InteractionRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: InteractionRepository::class)]
+class Interaction
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?bool $user_favorite = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $comment = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function isUserFavorite(): ?bool
+    {
+        return $this->user_favorite;
+    }
+
+    public function setUserFavorite(bool $user_favorite): static
+    {
+        $this->user_favorite = $user_favorite;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(string $comment): static
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+}
