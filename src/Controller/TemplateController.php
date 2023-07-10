0684aeeac91db32582724b99e3143edf13ae1405
monicaGY <monicagarciayadaicela@gmail.com>
2023-07-10 16:32:53 +0200

    Arreglo

--- src/Controller/TemplateController.php
+++ src/Controller/TemplateController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 class TemplateController extends AbstractController
 {
     #[Route('/template', name: 'template')]
