1c6d3ca78aeb68b38303856a7e8fab4ff77715bc
monicaGY <monicagarciayadaicela@gmail.com>
2023-07-10 16:20:02 +0200

    Enrutamiento

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,18 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
 {
-    #[Route('/post', name: 'app_post')]
-    public function index(): Response
+
+    #[Route('/post/{id}', name: 'post')]
+    public function index(int $id): Response
     {
         return $this->render('post/index.html.twig', [
             'controller_nam' => 'PostController!',
-            'array_asociativo' => ['despedida' => 'Adioos', 'nombre' => 'Esteban']
+            'array_asociativo' => ['despedida' => 'Adioos', 'nombre' => 'Esteban'],
+            'id' => $id
         ]);
     }
 }
---
+++ src/Controller/TemplateController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TemplateController extends AbstractController
+{
+    #[Route('/template', name: 'template')]
+    public function index(): Response
+    {
+        return $this->render('template/index.html.twig', [
+            'controller_name' => 'TemplateController',
+        ]);
+    }
+
+    #[Route('/template/parametro/{id}/{palabra}', name: 'template_parametros', 
+    defaults: ['id' => -1])]
+    public function parametros(int $id , string $palabra = "vacío"): Response
+    {
+
+        if($id < 0 ){
+            return $this->redirectToRoute('template_excepcion');
+
+            // return $this->redirect('http://symfony.com/doc');
+        }
+        
+        die("parametro id = {$id}; palabra = {$palabra}");
+    }
+
+    #[Route('/template/excepcion', name: 'template_excepcion')]
+    public function excepcion(): Response
+    {
+        // throw $this->createNotFoundException('Esta URL no esta disponible');
+        throw new NotFoundHttpException('Esta URL no esta disponible !');
+    }
+}
